3690653f8cd380b9fe1e670a95b6d430816a143d
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-05-29 13:53:53 -0500

    stefan corrected

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+    divisible_by_3_and_5 = []
+    for i in 1..limit-1
+        if i % 3 == 0
+            divisible_by_3_and_5 << i
+        elsif
+            i % 5 == 0
+            divisible_by_3_and_5 << i
+        else
+            0
+        end 
+    end
+    divisible_by_3_and_5
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
+
+
+
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,32 @@
 # Enter your object-oriented solution here!
+require 'pry'
+class Multiples
+    attr_accessor :number
+
+    def initialize(number)
+        @number = number
+    end
+
+    def collect_multiples
+        array_of_nums = [*1...self.number]
+        divisible_by_3_and_5 = []
+        array_of_nums.each do |num|
+            if num % 3 == 0
+                divisible_by_3_and_5 << num
+            elsif num % 5 == 0
+                divisible_by_3_and_5 << num
+            else
+                0
+            end
+        end
+        divisible_by_3_and_5
+    end
+    
+    def sum_multiples
+        self.collect_multiples.sum
+    end
+
+    
+end
+binding.pry
+0
