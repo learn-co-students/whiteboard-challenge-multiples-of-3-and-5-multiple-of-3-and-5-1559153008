2dd579829c2ff6919bcad21007b0cc0d233e7606
BobNarwhalKing <danceflurry4366@gmail.com>
2019-05-29 13:31:21 -0500

    messup

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,19 +1,28 @@
 # Enter your object-oriented solution here!
 
-def collect_multiples(num)
-    ans_arr = []
-    testing_nums = (1...num)
-    testing_nums.each { |numbers|
-        if (numbers % 3 == 0 || numbers % 5 == 0)
-            ans_arr << numbers
-        end
-    }
-    return ans_arr
-end
+class Multiples
 
-def sum_multiples(num)
-    numbers = collect_multiples(num)
-    answer = 0
-    numbers.each {|numbers| answer += numbers}
-    return answer
+    attr_reader :num
+
+    def initialize(num)
+        @num = num
+    end
+
+    def collect_multiples
+        ans_arr = []
+        testing_nums = (1...self.num)
+        testing_nums.each { |numbers|
+            if (numbers % 3 == 0 || numbers % 5 == 0)
+                ans_arr << numbers
+            end
+        }
+        return ans_arr
+    end
+
+    def sum_multiples
+        numbers = self.collect_multiples
+        answer = 0
+        numbers.each {|numbers| answer += numbers}
+        return answer
+    end
 end
