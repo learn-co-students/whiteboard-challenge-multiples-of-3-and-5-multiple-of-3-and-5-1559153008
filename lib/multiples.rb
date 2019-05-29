14d62cf01e281bea92bf1b98d668f1da1b9af55c
Brendan Brown <pbbrown1112@gmail.com>
2019-05-29 13:15:59 -0500

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+require 'pry'
+
+
+rng1 = (1..1000)
+arr1 = rng1.to_a
+
+def sum_multiples_of_3_and_5(arr)
+    three_and_five_arr = arr.select do | num |
+        num%3 == 0 || num%5 == 0
+    end
+    total = 0
+    three_and_five_arr.each do | number |
+        total += number
+    end
+    total
+
+end
+
+
+binding.pry
+0
