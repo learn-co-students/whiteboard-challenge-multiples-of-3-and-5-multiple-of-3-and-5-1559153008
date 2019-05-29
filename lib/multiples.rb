4c47a97b7622a7ceabe979c805805621591d9789
feven tsegay <feventsegay6@gmail.com>
2019-05-29 14:03:37 -0500

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,38 @@
-# Enter your procedural solution here!
+ require 'pry'
+class Multiple
+  def multiples
+    
+    #while num > 0 && num < 1000 do
+    num1= 0...1000
+    tot=0
+    num1.each do |num|
+        p num
+        if num % 3 ==0 || num % 5 ==0
+            tot +=num
+        end
+    end
+    tot
+  end
+  
+end
+
+multiples = Multiple.new
+
+
+
+
+
+
+  
+
+  binding.pry
+  
+
+0
+
+
+
+
+
+
+
