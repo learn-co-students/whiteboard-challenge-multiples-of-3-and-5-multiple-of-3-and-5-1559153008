6360e2a2dba60cd3ec6d4a92b0ee8738287da190
Ahawkfizz <001.aqh@gmail.com>
2019-05-29 13:49:15 -0500

    ariq

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,36 @@
 # Enter your procedural solution here!
+#require 'pry'
+class Multiples
+
+
+    def initialize()
+
+    end
+
+    
+    def collect_multiples(limit)
+        array = []
+        i = 1
+        loop do 
+            i += 1
+            if i%3==0 || i%5==0
+                if i == limit
+                    break
+                end
+                array << i
+            end
+        end
+        array 
+    end
+
+    def sum_multiples(limit)
+        sum = 0
+        collect_multiples(limit).sum
+
+    end
+
+end
+
+test = Multiples.new
+
+#binding.pry
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
 # Enter your object-oriented solution here!
+class OO_multiples
+def initialize()
+
+end
+
+
+def collect_multiples(limit)
+    array = []
+    i = 1
+    loop do 
+        i += 1
+        if i%3==0 || i%5==0
+            if i == limit
+                break
+            end
+            array << i
+        end
+    end
+    array 
+end
+
+def sum_multiples(limit)
+    sum = 0
+    collect_multiples(limit).sum
+
+end
+end
