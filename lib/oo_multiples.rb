89ced2aca64a4ea1868bf7b107d63d35180ff305
BobNarwhalKing <danceflurry4366@gmail.com>
2019-05-29 13:24:27 -0500

    Kurtis

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+
+def collect_multiples(num)
+    ans_arr = []
+    testing_nums = (1...num)
+    testing_nums.each { |numbers|
+        if (numbers % 3 == 0 || numbers % 5 == 0)
+            ans_arr << numbers
+        end
+    }
+    return ans_arr
+end
+
+def sum_multiples(num)
+    numbers = collect_multiples(num)
+    answer = 0
+    numbers.each {|numbers| answer += numbers}
+    return answer
+end
